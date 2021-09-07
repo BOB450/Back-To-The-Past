39b90d8ff04e42df49f6a746093602192e54611f
BOB450 <bwvoskuil@gmail.com>
2021-09-06 20:52:52 -0600

    added new tileset

--- Back To The Past/Assets/Scripts/BulletScript.cs
+++ Back To The Past/Assets/Scripts/BulletScript.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class BulletScript : MonoBehaviour
 {
 
+    void Start()
+    {
+        Invoke("des", 12);
+    }
+
     void OnCollisionEnter2D(Collision2D coll)
     {
         //Add damage later TODO
         Destroy(gameObject);
     }
 
-
+    void des()
+    {
+        Destroy(gameObject);
+    }
 }
